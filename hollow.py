4cf91c3bdce2a5a205b518165b815f15d5b26d45
Sean Sube <seansube@gmail.com>
2018-03-31 10:54:39 -0500

    retain tree, remove unmarked leaves, use comments

--- hollow.py
+++ hollow.py
@@ -1,10 +1,33 @@
 #! /usr/bin/env python3
 
 import argparse, sys
+from enum import Enum
 from ruamel.yaml import YAML
+
+class Next(Enum):
+  RECURSE = 1
+  REDACT = 2
+  RETAIN = 3
 
 parser = argparse.ArgumentParser(description="remove values from yaml document")
 parser.add_argument("-f", "--file", help="input file")
+
+# detect leaves
+# TODO: check comments
+def is_leaf(data, cmt):
+  dt = type(data)
+  #print("checking leaf: %s (%s)" % (data, dt))
+
+  if cmt != "" and "keep" in cmt:
+    return Next.RETAIN
+  elif dt is bool:
+    return Next.REDACT
+  elif dt is int:
+    return Next.REDACT
+  elif dt is str:
+    return Next.REDACT
+  else:
+    return Next.RECURSE
 
 # load a yaml document from file
 def load_doc(path, yaml):
@@ -19,29 +42,29 @@ def load_doc(path, yaml):
 
 # remove values, leaves, and marked branches
 def strip_branch(data):
-  stripped = dict()
-
   for key in data:
+    #print("checking %s" % key)
     val = data[key]
-    tval = type(val)
 
-    if tval is bool:
-      stripped[key] = False
-    elif tval is int:
-      stripped[key] = 0
-    elif tval is str:
-      stripped[key] = ""
+    if key in data.ca.items:
+      cmt = "".join([i.value for i in data.ca.items[key] if i != None])
+      #print("comment for %s: %s" % (key, cmt))
     else:
-      stripped[key] = strip_branch(val)
-  
-  return stripped
+      cmt = ""
+
+    next = is_leaf(val, cmt)
+
+    if next == Next.RECURSE:
+      strip_branch(val)
+    elif next == Next.REDACT:
+      data[key] = None
 
 def main(args):
   args = parser.parse_args(args)
 
   yaml = YAML()
   data = load_doc(args.file, yaml)
-  data = strip_branch(data)
+  strip_branch(data)
   yaml.dump(data, sys.stdout)
 
 if __name__ == "__main__":
