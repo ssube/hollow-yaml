104a1ba76fa09e41cec1168e4032d0ee66d8097e
ssube <seansube@gmail.com>
2019-09-07 11:06:35 -0500

    handle most lists correctly

--- hollow.py
+++ hollow.py
@@ -3,29 +3,52 @@
 import argparse, sys
 from enum import Enum
 from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedSeq
+from ruamel.yaml.tokens import CommentToken
 
 class Next(Enum):
   RECURSE = 1
   REDACT = 2
   RETAIN = 3
+  RELIST = 4
 
 parser = argparse.ArgumentParser(description="remove values from yaml document")
 parser.add_argument("-f", "--file", help="input file")
 
-# detect leaves
-# TODO: check comments
-def is_leaf(data, cmt):
+def empty_value(data):
   dt = type(data)
-  #print("checking leaf: %s (%s)" % (data, dt))
 
-  if cmt != "" and "keep" in cmt:
-    return Next.RETAIN
-  elif dt is bool:
+  if dt is bool:
+    return False
+  elif dt is int:
+    return 0
+  elif dt is str:
+    return ''
+  elif dt is CommentedSeq:
+    return []
+
+# detect leaves
+def is_leaf(data, cmt):
+  if cmt != "":
+    #print('checking leaf with comment', cmt)
+    if "@retain" in cmt:
+      return Next.RETAIN
+    elif "@redact" in cmt:
+      return Next.REDACT
+
+  dt = type(data)
+  #print("checking leaf type: %s (%s)" % (data, dt))
+
+  if dt is bool:
     return Next.REDACT
   elif dt is int:
     return Next.REDACT
   elif dt is str:
     return Next.REDACT
+  elif dt is CommentedSeq:
+    return Next.RELIST
+  elif dt is list:
+    return Next.RELIST
   else:
     return Next.RECURSE
 
@@ -40,24 +63,36 @@ def load_doc(path, yaml):
     print("Error loading YAML: %s" % err)
     sys.exit(1)
 
+def build_comment(items):
+  cmt = ""
+  for i in items:
+    it = type(i)
+    if it == list:
+      cmt += build_comment(i)
+    elif it == CommentToken:
+      cmt += i.value
+
+  return cmt.strip()
+
+def strip_list(data):
+  return []
+
 # remove values, leaves, and marked branches
 def strip_branch(data):
+  #print('stripping branch', type(data), data)
   for key in data:
-    #print("checking %s" % key)
     val = data[key]
+    cmt = build_comment(data.ca.items[key]) if key in data.ca.items else ""
 
-    if key in data.ca.items:
-      cmt = "".join([i.value for i in data.ca.items[key] if i != None])
-      #print("comment for %s: %s" % (key, cmt))
-    else:
-      cmt = ""
-
+    #print("checking data key: %s = %s (%s)" % (key, val, cmt))
     next = is_leaf(val, cmt)
 
     if next == Next.RECURSE:
       strip_branch(val)
+    elif next == Next.RELIST:
+      data[key] = strip_list(val)
     elif next == Next.REDACT:
-      data[key] = None
+      data[key] = empty_value(val)
 
 def main(args):
   args = parser.parse_args(args)
